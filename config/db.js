d3020065cfc798702e2948bee5acda1e549ce86a
Nguyen Dai Tai <taind1326@gmail.com>
2026-07-12 15:36:04 +0700

    Refactor database configuration

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const sql = require('mssql')
+
+const config = {
+    user: 'sa',
+    password: '123456',
+    server: 'localhost',
+    database: 'QUANLI_SACH',
+    port: 1433,
+    options: {trustServerCertificate: true}
+}
+
+
+async function connectDB() {
+    try{
+        await sql.connect(config)
+        console.log('Da ket noi server')
+    }
+    
+    catch(error){
+        console.log('Loi ket noi server', error.message)
+    }
+}
+
+
+module.export = {sql, connectDB}
